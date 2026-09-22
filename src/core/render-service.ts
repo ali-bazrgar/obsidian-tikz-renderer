cb3a04b0b6fb65216ffca884e0ab7ccf61b7dfb8
Ali Bazrgar <bazrgar20@gmail.com>
2026-09-22 22:50:04 +0330

    fix: preserve correct replacement arguments in math normalizer

--- src/core/render-service.ts
+++ src/core/render-service.ts
@@ -444,9 +444,9 @@ export function compilerArgs(tex: string, work: string, outputType: EnginePlan["
 }
 
 function normalizeLatexSource(source: string): string {
-  return source.replace(/^([ \t]*)\[[ \t]*\r?\n([\s\S]*?)\r?\n\\1\][ \t]*$/gmu, (whole, indent: string, inner: string, trailing: string) => {
+  return source.replace(/^([ \t]*)\[[ \t]*\r?\n([\s\S]*?)\r?\n\\1\][ \t]*$/gmu, (whole, indent: string, inner: string) => {
     if (!/(?:\\\\(?:frac|dfrac|tfrac|sqrt|sum|prod|int|lim|left|right|text|mathrm|mathbf|operatorname|begin\\{|end\\{)|[_^]|[=<>])/.test(inner)) return whole;
-    return indent + "\\[\n" + inner + "\n" + indent + "\\]" + trailing;
+    return indent + "\\[\n" + inner + "\n" + indent + "\\]";
   });
 }
 
